00e59933f4c9356739810e2dbc39f6e0aa32ba93
gwengz <gwengozesuperb@gmail.com>
2025-11-21 18:09:08 +0800

    Editing form appearance (fonts and design)

--- SQL_Crud/Form1.Designer.vb
+++ SQL_Crud/Form1.Designer.vb
@@ -23,10 +23,18 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ButtonConnect = New System.Windows.Forms.Button()
+        Me.TextBoxName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxAge = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxEmail = New System.Windows.Forms.TextBox()
+        Me.ButtonCreate = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'ButtonConnect
         '
+        Me.ButtonConnect.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ButtonConnect.Location = New System.Drawing.Point(12, 12)
         Me.ButtonConnect.Name = "ButtonConnect"
         Me.ButtonConnect.Size = New System.Drawing.Size(163, 76)
@@ -34,17 +42,94 @@ Partial Class Form1
         Me.ButtonConnect.Text = "Connect MySQL"
         Me.ButtonConnect.UseVisualStyleBackColor = True
         '
+        'TextBoxName
+        '
+        Me.TextBoxName.Location = New System.Drawing.Point(75, 118)
+        Me.TextBoxName.Name = "TextBoxName"
+        Me.TextBoxName.Size = New System.Drawing.Size(156, 20)
+        Me.TextBoxName.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("PT Bold Stars", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 121)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 19)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("PT Bold Stars", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 147)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 19)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Age:"
+        '
+        'TextBoxAge
+        '
+        Me.TextBoxAge.Location = New System.Drawing.Point(75, 144)
+        Me.TextBoxAge.Name = "TextBoxAge"
+        Me.TextBoxAge.Size = New System.Drawing.Size(156, 20)
+        Me.TextBoxAge.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("PT Bold Stars", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 173)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 19)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "E-mail:"
+        '
+        'TextBoxEmail
+        '
+        Me.TextBoxEmail.Location = New System.Drawing.Point(75, 170)
+        Me.TextBoxEmail.Name = "TextBoxEmail"
+        Me.TextBoxEmail.Size = New System.Drawing.Size(156, 20)
+        Me.TextBoxEmail.TabIndex = 5
+        '
+        'ButtonCreate
+        '
+        Me.ButtonCreate.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.ButtonCreate.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonCreate.Location = New System.Drawing.Point(149, 211)
+        Me.ButtonCreate.Name = "ButtonCreate"
+        Me.ButtonCreate.Size = New System.Drawing.Size(82, 37)
+        Me.ButtonCreate.TabIndex = 7
+        Me.ButtonCreate.Text = "CREATE (Insert data)"
+        Me.ButtonCreate.UseVisualStyleBackColor = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(328, 304)
+        Me.Controls.Add(Me.ButtonCreate)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBoxEmail)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBoxAge)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBoxName)
         Me.Controls.Add(Me.ButtonConnect)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ButtonConnect As Button
+    Friend WithEvents TextBoxName As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxAge As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBoxEmail As TextBox
+    Friend WithEvents ButtonCreate As Button
 End Class
--- SQL_Crud/Form1.vb
+++ SQL_Crud/Form1.vb
@@ -17,4 +17,7 @@ Public Class Form1
         End Try
     End Sub
 
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
 End Class
